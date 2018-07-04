b1b560d2cd19ad4bd41384092585287748170ffe
threough <michael.r.smith@siu.edu>
2018-07-04 07:52:34 -0500

    fixing bug in instruction mapping

--- chip8.c
+++ chip8.c
@@ -75,11 +75,12 @@ void cycle(chip8 *machine) {
     /* decode and execute the opcode*/
     switch (opcode >> 12) {
         case 0x0: 
-            switch (opcode & 0xf) {
+            switch (opcode & 0xff) {
                 case 0xe0: CLS(machine); break;
                 case 0xee: RET(machine); break;
-                default: printf("UNKNOWN INSTRUCTION\n"); break;
+                default: printf("UNKNOWN INSTRUCTION 0\n"); break;
             }
+            break;
         case 0x1: JP_ADDR(machine); break;
         case 0x2: CALL_ADDR(machine); break;
         case 0x3: SE_REG_BYTE(machine); break;
@@ -88,7 +89,7 @@ void cycle(chip8 *machine) {
         case 0x6: LD_REG_BYTE(machine); break;
         case 0x7: ADD_REG_BYTE(machine); break;
         case 0x8: 
-            switch (opcode & 0x3) {
+            switch (opcode & 0xf) {
                 case 0x0: LD_REG_REG(machine); break;
                 case 0x1: OR_REG_REG(machine); break;
                 case 0x2: AND_REG_REG(machine); break;
@@ -98,21 +99,23 @@ void cycle(chip8 *machine) {
                 case 0x6: SHR(machine); break;
                 case 0x7: SUBN(machine); break;
                 case 0xe: SHL(machine); break;
-                default: printf("UNKNOWN INSTRUCTION\n"); break;
+                default: printf("UNKNOWN INSTRUCTION 8\n"); break;
             }
+            break;
         case 0x9: SNE_REG_REG(machine); break;
         case 0xa: LD_I_ADDR(machine); break;
         case 0xb: JP_V0_ADDR(machine); break;
         case 0xc: RND(machine); break;
         case 0xd: DRW(machine); break;
         case 0xe: 
-            switch (opcode & 0xf) {
+            switch (opcode & 0xff) {
                 case 0x9e: SKP(machine); break;
                 case 0xa1: SKNP(machine); break;
-                default: printf("UNKNOWN INSTRUCTION\n"); break;
+                default: printf("UNKNOWN INSTRUCTION E\n"); break;
             }
+            break;
         case 0xf: 
-            switch (opcode & 0xf) {
+            switch (opcode & 0xff) {
                 case 0x07: LD_REG_DT(machine); break;
                 case 0x0a: LD_REG_K(machine); break;
                 case 0x15: LD_DT_REG(machine); break;
@@ -122,8 +125,9 @@ void cycle(chip8 *machine) {
                 case 0x33: LD_B_REG(machine); break;
                 case 0x55: LD_I_REG(machine); break;
                 case 0x65: LD_REG_I(machine); break;
-                default: printf("UNKNOWN INSTRUCTION\n"); break;
+                default: printf("UNKNOWN INSTRUCTION F\n"); break;
             }
+            break;
         default: printf("UNKNOWN INSTRUCTION\n"); break;
     }
 
@@ -135,6 +139,9 @@ void cycle(chip8 *machine) {
 
 int main() {
     chip8 *machine = initialize("pong.rom");
+    for (int i = 0; i < 123; i++) {
+        cycle(machine);
+    }
 
     printf("%d\n", machine->sound_timer);
     return 0;
--- instructions.c
+++ instructions.c
@@ -1,36 +1,37 @@
 #include "instructions.h"
+#include <stdio.h>
 
-void CLS(chip8 *machine) { }
-void RET(chip8 *machine) { }
-void JP_ADDR(chip8 *machine) { }
-void CALL_ADDR(chip8 *machine) { }
-void SE_REG_BYTE(chip8 *machine) { }
-void SNE_REG_BYTE(chip8 *machine) { }
-void SE_REG_REG(chip8 *machine) { }
-void LD_REG_BYTE(chip8 *machine) { }
-void ADD_REG_BYTE(chip8 *machine) { }
-void LD_REG_REG(chip8 *machine) { }
-void OR_REG_REG(chip8 *machine) { }
-void AND_REG_REG(chip8 *machine) { }
-void XOR_REG_REG(chip8 *machine) { }
-void ADD_REG_REG(chip8 *machine) { }
-void SUB_REG_REG(chip8 *machine) { }
-void SHR(chip8 *machine) { }
-void SUBN(chip8 *machine) { }
-void SHL(chip8 *machine) { }
-void SNE_REG_REG(chip8 *machine) { }
-void LD_I_ADDR(chip8 *machine) { }
-void JP_V0_ADDR(chip8 *machine) { }
-void RND(chip8 *machine) { }
-void DRW(chip8 *machine) { }
-void SKP(chip8 *machine) { }
-void SKNP(chip8 *machine) { }
-void LD_REG_DT(chip8 *machine) { }
-void LD_REG_K(chip8 *machine) { }
-void LD_DT_REG(chip8 *machine) { }
-void LD_ST_REG(chip8 *machine) { }
-void ADD_I_REG(chip8 *machine) { }
-void LD_F_REG(chip8 *machine) { }
-void LD_B_REG(chip8 *machine) { }
-void LD_I_REG(chip8 *machine) { }
-void LD_REG_I(chip8 *machine) { }
+void CLS(chip8 *machine) { printf("CLS\n"); }
+void RET(chip8 *machine) { printf("RET\n"); }
+void JP_ADDR(chip8 *machine) { printf("JP_ADDR\n"); }
+void CALL_ADDR(chip8 *machine) { printf("CALL_ADDR\n"); }
+void SE_REG_BYTE(chip8 *machine) { printf("SE_REG_BYTE\n"); }
+void SNE_REG_BYTE(chip8 *machine) { printf("SNE_REG_BYTE\n"); }
+void SE_REG_REG(chip8 *machine) { printf("SE_REG_REG\n"); }
+void LD_REG_BYTE(chip8 *machine) { printf("LD_REG_BYTE\n"); }
+void ADD_REG_BYTE(chip8 *machine) { printf("ADD_REG_BYTE\n"); }
+void LD_REG_REG(chip8 *machine) { printf("LD_REG_REG\n"); }
+void OR_REG_REG(chip8 *machine) { printf("OR_REG_REG\n"); }
+void AND_REG_REG(chip8 *machine) { printf("AND_REG_REG\n"); }
+void XOR_REG_REG(chip8 *machine) { printf("XOR_REG_REG\n"); }
+void ADD_REG_REG(chip8 *machine) { printf("ADD_REG_REG\n"); }
+void SUB_REG_REG(chip8 *machine) { printf("SUB_REG_REG\n"); }
+void SHR(chip8 *machine) { printf("SHR\n"); }
+void SUBN(chip8 *machine) { printf("SUBN\n"); }
+void SHL(chip8 *machine) { printf("SHL\n"); }
+void SNE_REG_REG(chip8 *machine) { printf("SNE_REG_REG\n"); }
+void LD_I_ADDR(chip8 *machine) { printf("LD_I_ADDR\n"); }
+void JP_V0_ADDR(chip8 *machine) { printf("JP_V0_ADDR\n"); }
+void RND(chip8 *machine) { printf("RND\n"); }
+void DRW(chip8 *machine) { printf("DRW\n"); }
+void SKP(chip8 *machine) { printf("SKP\n"); }
+void SKNP(chip8 *machine) { printf("SKNP\n"); }
+void LD_REG_DT(chip8 *machine) { printf("LD_REG_DT\n"); }
+void LD_REG_K(chip8 *machine) { printf("LD_REG_K\n"); }
+void LD_DT_REG(chip8 *machine) { printf("LD_DT_REG\n"); }
+void LD_ST_REG(chip8 *machine) { printf("LD_ST_REG\n"); }
+void ADD_I_REG(chip8 *machine) { printf("ADD_I_REG\n"); }
+void LD_F_REG(chip8 *machine) { printf("LD_F_REG\n"); }
+void LD_B_REG(chip8 *machine) { printf("LD_B_REG\n"); }
+void LD_I_REG(chip8 *machine) { printf("LD_I_REG\n"); }
+void LD_REG_I(chip8 *machine) { printf("LD_REG_I\n"); }
